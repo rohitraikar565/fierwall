5b0805b74174cffb7429f71ffd8a4dbea4ffc5e2
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2022-04-01 07:43:27 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "module_dependency" {
+  type        = "string"
+  default     = ""
+  description = "This is a dummy value to great module dependency"
+}
+
+variable "project" {
+    description = "The project in which the resource belongs. If it is not provided, the provider project is used."
+    type        = "string"
+    default     = ""
+}
+
+variable "vpc" {
+    description = "Name of VPC"
+    type        = "string"
+}
+
+/* Maps with all the configurations */
+variable "egress" {
+    description = "Configurtion for egress rules"
+    type        = "map"
+    default     = {}
+}
+
+variable "ingress" {
+    description = "Configurtion for ingress rules"
+    type        = "map"
+    default     = {}
+}
